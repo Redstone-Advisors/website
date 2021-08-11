1e93fa30cc7a4cdda04be18ee679a1c7235eb820
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2021-08-10 23:33:31 -0500

    home content and styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,18 +3,45 @@ import styled from "styled-components";
 const Nav = styled.nav`
   font-size: 32px;
   color: white;
-  padding:0;
-  margin:0
+  padding:1rem;
+  margin:0;
   display:block;
   position:fixed;
   top:0;
-  padding:1rem;
+left:0;
+right:0;
+z-index:99;
+  display:flex;
+  justify-content: space-between;
 `;
+const NavList = styled.ul`
+display: block;
+width: max-content;
+list-style:none;
+display:flex;
+padding:0;
+margin:0;
+li{
+    font-size: 1.5rem;
+    margin-right: 2rem;
+    &:last-child{
+        margin-right: 0;
+    }
+}
+`
+
 
 const Navbar = () => {
     return (
         <Nav> 
-            <h1>Redstone Advisors</h1>
+            <div>Redstone Advisors</div>
+            <NavList>
+                <li>Who We Are</li>
+                <li>What We Do</li>
+                <li>Why Redstone</li>
+                <li>Blog</li>
+                <li>FAQ</li>
+            </NavList>
         </Nav>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from "styled-components";
 import background from '../../assets/background.mp4'
 const Wrapper = styled.section`
-  min-height:100vh
+  min-height:100vh;
   width:100%;
   overflow:hidden;
 `;
@@ -10,13 +10,44 @@ const Video = styled.video`
 display:block;
 height:100vh;
 `;
+const Headline = styled.h1`
+color: #fff;
+position: absolute;
+line-height: 5rem;
+font-size: 3rem;
+top:40%;
+left: 50%;
+transform: translate(-50%, -50%);
+`
+const Teaser = styled.div`
+color: #fff;
+position: absolute;
+bottom:10%;
+left: 50%;
+transform:translate(-50%, -50%);
+h2{
+    margin-bottom: 1rem;
+    font-size: 2rem;
+    text-align: center;
+}
+span{
+    font-size: 1.5rem;
+    margin-right: 2rem;
+    &:last-child{
+        margin-right: 0;
+    }
+}
+`
 const Home = () => {
     return (
         <Wrapper>
-       <Video autoPlay loop muted id="videoBg">
-           <source src={background} type='video/mp4'/>
-       </Video>
-
+            <Video autoPlay loop muted id="videoBg">
+                <source src={background} type='video/mp4' />
+            </Video>
+            <Headline>You created the wealth to retire<br></br>Now, which path will you take?</Headline>
+            <Teaser><h2>May We Reccomend The Road Less Traveled</h2>
+                <div><span>Wealth Preservation</span><span>Income Optimizaton</span><span>Investment Specialization</span></div>
+            </Teaser>
         </Wrapper>
     )
 }
